d218f4453d0de9707f26c78ebe0cec96cd5d351b
ihsan-hepsen <ihsanhepsen@gmail.com>
2023-08-01 02:41:20 -0500

    fixed typo

--- components/static/AboutSection.tsx
+++ components/static/AboutSection.tsx
@@ -7,7 +7,7 @@ export default function AboutSection() {
                 ...full-stack software engineer.
             </h3>
             <p>
-                I'm a a full-stack software engineer more focused on backend. I have experience working with various technologies.
+                I'm a full-stack software engineer more focused on backend. I have experience working with various technologies.
                 Scroll down to see the full list 👇 
             </p>
         </section>
